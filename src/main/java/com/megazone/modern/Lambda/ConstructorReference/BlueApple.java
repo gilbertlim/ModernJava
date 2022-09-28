f7941961c1ef7734ff44624b692684910384e96c
gilbertlim <9ilbert.app@gmail.com>
2022-09-28 16:25:58 +0900

    Add Constructor Reference and Method Referece

---
+++ src/main/java/com/megazone/modern/Lambda/ConstructorReference/BlueApple.java
@@ -0,0 +1,7 @@
+package com.megazone.modern.Lambda.ConstructorReference;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class BlueApple {
+}
---
+++ src/main/java/com/megazone/modern/Lambda/ConstructorReference/GreenApple.java
@@ -0,0 +1,11 @@
+package com.megazone.modern.Lambda.ConstructorReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+public class GreenApple {
+    public Integer weight;
+}
