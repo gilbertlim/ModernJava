bab0da99a5e6dc948ae25ec1f554bacaa2673873
gilbertlim <9ilbert.app@gmail.com>
2022-09-30 11:49:21 +0900

    Apply Slf4j

---
+++ src/main/java/com/megazone/modern/Stream/Dish.java
@@ -0,0 +1,15 @@
+package com.megazone.modern.Stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class Dish {
+
+    private String name;
+    private Integer calories;
+
+}
